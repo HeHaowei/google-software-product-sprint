4ac8b7220b55f8a24086144a91a0b3e15ca0ef8f
HeHaowei <e0253738@u.nus.edu>
2020-06-26 01:25:36 +0800

    Add comments to function in FindMeetingQuery.java

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -23,13 +23,23 @@ import java.util.List;
 import java.util.Set;
 
 public final class FindMeetingQuery {
+
+  /**
+   * query is to query the available TimeRange for a given request and events list
+   * Input: Collection of events, MeetingRequest
+   * Output: Collection of available TimeRange 
+   */
+
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-      
+
     ArrayList<TimeRange> event_time = new ArrayList<TimeRange>();
     Set<String> request_attendees = new HashSet<String>(request.getAttendees());
     for (Event event: events) {
         Set<String> intersection_attendees = new HashSet<String>(request.getAttendees());
         Set<String> event_attendees = new HashSet<String>(event.getAttendees());
+
+        // Check whether the event has intersection of attendees with the request
+        // If there is intersection, the request must not overlap with the event time
         intersection_attendees.retainAll(event_attendees);
         if (!intersection_attendees.isEmpty()) {
             event_time.add(event.getWhen());
@@ -37,6 +47,21 @@ public final class FindMeetingQuery {
     }
     return queryByTime(mergeEventTime(event_time), request.getDuration());
   }
+
+  /**
+   * mergeEventTime is to merge the overlap intervals.
+   * Input: the list of event_time
+   * Output: merged and sorted list of event_time
+   * e.g. [1,3], [2,8], [4,5], [9,10] will be merged into [1,8],[9,10] 
+   * [a, b] means [start_time, end_time]
+   * The algorithm is: sort the start_time and end_time of the event in ascending order separately
+   * Then the above example will become:
+   * start_time: (1, 2, 4, 9)
+   * end_time: (3, 5, 8, 10)
+   * For i from 0 to the size of the eventList, if start_time[i + 1] > end_time[i] or i = size - 1, meaning
+   * there generates an interval: [start_time[j], end_time[i]], j is the starting point of the interval
+   * and j will be updated to i + 1 after each merge operation 
+   */
 
   private ArrayList<TimeRange> mergeEventTime(ArrayList<TimeRange> event_time) {
 
@@ -59,6 +84,17 @@ public final class FindMeetingQuery {
       return result;
 
   }
+
+  /**
+   * queryByTime is to query the availabe TimeRange given the merged and sorted event_time list 
+   * and the duration of the request event
+   * Input: ArrayList of merged and sorted event_time list and the duration of the request event
+   * Output: Collection of available TimeRange 
+   * e.g: the event_time is [2,5) [8, 10) [14, 20) [21, 22) and the full range is [0, 24]
+   *      and the request duration is 2 
+   *      [a, b] or [a, b) is [start_time, end_time] or [start_time, end_time)
+   *      Thus, the output result will be [0, 2), [5, 8), [10,14), (22, 24] 
+   */
  
     private Collection<TimeRange> queryByTime(ArrayList<TimeRange> event_time, long duration) {
 
